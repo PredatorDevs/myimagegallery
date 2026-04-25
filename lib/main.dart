3c15f0962e339e20ff85abca759453adc79339c7
PredatorDevs <48956818+PredatorDevs@users.noreply.github.com>
2026-04-25 11:11:52 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Utilidades Personales',
+      title: 'Mis Archivos',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
